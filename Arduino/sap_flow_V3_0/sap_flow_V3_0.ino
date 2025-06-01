583a3f2ba791a9c4e66c9ce394d8809d76ca8cc2
terrabite3 <terrabite3@gmail.com>
2025-06-01 14:57:04 -0400

    Extract SD code

--- Arduino/sap_flow_V3_0/sap_flow_V3_0.ino
+++ Arduino/sap_flow_V3_0/sap_flow_V3_0.ino
@@ -1,6 +1,5 @@
 
 #include <RTClib.h>
-#include <SD.h>
 
 #define SW_AUX A2
 
@@ -9,70 +8,15 @@
 #ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
   #define KEEP_ON D0
   #define HEAT D1
-  
-  #define SD_CS 23
-  #define SD_CLK 18
-  #define SD_MOSI 19
-  #define SD_MISO 20
-  #define SD_CARD_DETECT 16
 #endif
 
 #ifdef ADAFRUIT_FEATHER_M0
   #define KEEP_ON 1
   #define HEAT 0
-  
-  #define SD_CS 4
-  #define SD_CARD_DETECT 7
 #endif
-
-
-bool setupSD()
-{
-  if (digitalRead(SD_CARD_DETECT))
-  {
-    Serial.println("SD card inserted");
-  }
-  else
-  {
-    Serial.println("SD card not inserted");
-  }
-#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
-  auto result = SD.begin(SD_CS, SPI1);
-#endif
-#ifdef ADAFRUIT_FEATHER_M0
-  auto result = SD.begin(SD_CS);
-#endif
-
-  if (!result)
-  {
-    Serial.println("SD card not started");
-    return false;
-  }
-  else
-  {
-    Serial.println("SD card started");
-    return true;
-  }
-}
-
-void appendToSD(const char* str, int id)
-{
-  char filename[] = "testNNN.txt";
-  filename[4] = '0' + (id / 100) % 10;
-  filename[5] = '0' + (id / 10) % 10;
-  filename[6] = '0' + id % 10;
-  Serial.printf("filename: %s\n", filename);
-
-  File f = SD.open(filename, FILE_WRITE);
-  f.println(str);
-  f.close();
-}
-
-
 
 
 RTC_DS3231 rtc;
-
 
 
 void setup() {
@@ -93,7 +37,6 @@ void setup() {
 
   pinMode(SW_AUX, INPUT_PULLUP);
 
-  pinMode(SD_CARD_DETECT, INPUT_PULLUP);
 }
 
 void loop() {
