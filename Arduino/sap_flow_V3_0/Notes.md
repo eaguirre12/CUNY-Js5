4f85d9bf091fc02b17e449a4ac52030b0fd2c8d4
terrabite3 <terrabite3@gmail.com>
2025-06-01 13:45:24 -0400

    Duplicate turn-on sketch in preparation for refactoring

---
+++ Arduino/sap_flow_V3_0/Notes.md
@@ -0,0 +1,169 @@
+## 2025-05-27
+
+Got the first boards back. 
+    I soldered it up
+    I haven't noticed any issues yet
+
+
+I plugged it into USB-C for the first time, and it's sweeping the RGB LED through the hues.
+
+I've got hello world!
+
+
+
+Let's test with a 12V battery pack
+    Works great
+
+
+
+## 2025-05-28
+
+I've noticed that when I power from the battery, it makes a whine
+    The whine changes as the LEDs turn on and off.
+    When I connect USB only, there's no whine. When I connect USB and hold the Turn On button, there is a quieter whine.
+    I bet this is caused by back-feeding the regulator. I checked the datasheet for the RT9080 and it doesn't say anything about backfeeding.
+        To start, the OUT pin is higher than the IN pin. The parasitic diode of the regulator's transistor allows current to flow from OUT to IN.
+        The input capacitor charges. This pulls the EN pin high, which turns on the regulator.
+        This is my guess at least.
+    Maybe I can solve this by pulling the EN pin low
+        It made the whine slightly quieter, but didn't fix it
+    https://learn.adafruit.com/adafruit-feather-rp2040-adalogger/power-management#alternative-power-options-3122391
+        Another option is to use a 5V regulator to the USB pin, with a diode. The diode will drop the voltage slightly, enough to hopefully prevent the regulator from backfeeding USB to the PC. But the drop will be small enough that the regulator still has plenty of headroom.
+    Maybe the whine is harmless?
+        It won't affect power efficiency in the field (if it is at all) because we only use USB on the bench.
+
+
+
+Keep-on
+    Not working
+    Turns out the RP2040 Adalogger has a different pinout!
+
+
+
+Is it possible for the battery sense to turn on the uC?
+    It would be at 1.2V nominal...
+
+
+I'm reading "373" for the battery voltage
+    Is that out of 1024?
+    Is the reference 3.3V?
+    Yes and yes
+    Reading 13.2V
+
+
+
+Turn-on checklist
+    LEDs - done
+    Keep-on pin - done
+    Neopixel - done
+    RTC - done
+        Setting time - done
+        Wake up with alarm - done
+    ADC - done
+        First measurement - done
+        Mux - done
+        Verify settling time - off-by-1 error (1 SI prefix!)
+    Heater - done
+        Turn on - done
+        Measure current - doesn't work very well
+    SD card
+        Write to file
+        Read back file and dump to serial
+
+
+
+The ADC was super noisy, sometimes going haywire.
+    Answer: a bad jumper wire!
+
+
+I miscalculated the settling time for the ADC
+    With 1 nF cap, it's 5 us, not 5 ms!
+    I've changed the schematic to 100 nF
+    The ADS1100 clock is nominally 275 kHz. So my actual antialiasing filter cutoff of 200 kHz is too high!
+    I added 100nF on top and the values seem slightly less noisy, but I could be imagining it. 
+    The values are holding steady +/- 1, before it was like +/- 2.
+    1 is 0.3 mV, so this is pretty negligible either way
+
+
+The current sense resistor is too small to get a good read on an LED.
+    It doesn't even work all that well with a 75 Ohm resistor
+    I should use 1 Ohm instead. Schematic changed.
+
+
+## 2025-05-31
+
+What's left to turn on?
+    SD card
+    Button
+    Calculate voltage/current of the heater NFET
+
+
+Heater NFET calculations
+    If the current is 275 mA, then voltage drop across the sense resistor will be 275 mV
+    That means V_GS = 3.3 - 0.275 = 3.025
+    R_ON = 48 mOhm max with V_GS = 2.5V
+    P = 275mA ^2 * 48 mOhm = 4 mW
+
+
+Should I power the Adalogger through the USB pin instead?
+    The risk is back-feeding power to USB
+    I could use a 5V regulator on board, with a diode to the USB pin. That would prevent the USB feeding the battery side of the circuit when no battery is attached. And it would drop the voltage slightly.
+    Eh, I think I'll stick with what I have. It seems to be working.
+
+
+
+What changes to make?
+    Increase sense resistor (done)
+    Change antialiasing filter cap (done)
+    Increase resistor for yellow and green LEDs (done)
+    Add reverse polarity protection (done)
+    Add a heater LED (done)
+
+
+Heater LED
+    With forward voltage of 1.8V, the existing red LEDs from 3.3V have 0.5 mA current
+    Running from 12V, we want 22k
+
+
+
+I should test this with the M0 Adalogger
+    Needed a few changes, but it works
+
+
+The Aux switch is working
+
+
+
+Could I add a bunch of solder jumpers to set the logger number?
+    I have 3 ADC pins free. If I had a 10-voltage divider, that could be used for a 3-digit number.
+    Soldering 3 solder jumpers during assembly wouldn't be much work. Comparable to updating the number in the code when uploading. And much more permanent.
+
+
+## 2025-06-01
+
+Bodging the 3.0 version to work with the ID
+    900
+        A3 to 3.3
+        A4 to GND
+        A5 to GND
+    990
+        A3 to 3.3
+        A4 to 3.3
+        A5 to GND
+    999
+        A3 to 3.3
+        A4 to 3.3
+        A5 to 3.3
+
+
+I also replaced the 20 Ohm sense resistor with 1 Ohm on 900, 990, and 999
+
+
+Ah, here's a problem. The RP2040 only has 4 analog pins!
+    I can free up one analog pin used by the button. The RP2040 has an extra 
+
+
+
+Continuing on these notes, duplicated from `turn_on_V3_0`
+
+Refactoring the code into several files
---
+++ Arduino/sap_flow_V3_0/sap_flow_V3_0.ino
@@ -0,0 +1,496 @@
+
+#include <RTClib.h>
+#include <SD.h>
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+#include <Adafruit_NeoPixel.h>
+#endif
+
+
+#define LED_R 10
+#define LED_Y 11
+#define LED_G 12
+#define LED_FEATHER 13
+#define LED_POWER 5
+#define LED_TIMER 6
+#define LED_ERROR 9
+
+#define V_BAT_SENSE A0
+#define HEATER_SENSE A1
+#define SW_AUX A2
+
+
+// TX pin is different between these two boards
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+  #define KEEP_ON D0
+  #define HEAT D1
+
+  #define MUX_S0 14
+  #define MUX_S1 15
+  #define MUX_S2 8
+  
+  #define SD_CS 23
+  #define SD_CLK 18
+  #define SD_MOSI 19
+  #define SD_MISO 20
+  #define SD_CARD_DETECT 16
+  
+  // The RP2040 doesn't have A4 and A5, so for now just read A3 for all three
+  #define ID_0 A3
+  #define ID_1 A3
+  #define ID_2 A3
+#endif
+
+#ifdef ADAFRUIT_FEATHER_M0
+  #define KEEP_ON 1
+  #define HEAT 0
+
+  #define MUX_S0 SCK
+  #define MUX_S1 MOSI
+  #define MUX_S2 MISO
+  
+  #define SD_CS 4
+  #define SD_CARD_DETECT 7
+  
+  #define ID_0 A5
+  #define ID_1 A4
+  #define ID_2 A3
+#endif
+
+
+bool setupSD()
+{
+  if (digitalRead(SD_CARD_DETECT))
+  {
+    Serial.println("SD card inserted");
+  }
+  else
+  {
+    Serial.println("SD card not inserted");
+  }
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+  auto result = SD.begin(SD_CS, SPI1);
+#endif
+#ifdef ADAFRUIT_FEATHER_M0
+  auto result = SD.begin(SD_CS);
+#endif
+
+  if (!result)
+  {
+    Serial.println("SD card not started");
+    return false;
+  }
+  else
+  {
+    Serial.println("SD card started");
+    return true;
+  }
+}
+
+void appendToSD(const char* str, int id)
+{
+  char filename[] = "testNNN.txt";
+  filename[4] = '0' + (id / 100) % 10;
+  filename[5] = '0' + (id / 10) % 10;
+  filename[6] = '0' + id % 10;
+  Serial.printf("filename: %s\n", filename);
+
+  File f = SD.open(filename, FILE_WRITE);
+  f.println(str);
+  f.close();
+}
+
+// #define ADC_ADDRESS 0b1001'000
+#define ADC_ADDRESS 0x48
+Adafruit_I2CDevice adc_i2c(ADC_ADDRESS, &Wire);
+
+bool setup_adc()
+{
+  if (!adc_i2c.begin())
+  {
+    Serial.println("Couldn't find ADS1100");
+    return false;
+  }
+
+  return true;
+}
+
+int16_t read_adc(int mux_channel, int settle_delay)
+{
+  digitalWrite(MUX_S0, mux_channel & 1);
+  digitalWrite(MUX_S1, mux_channel & 2);
+  digitalWrite(MUX_S2, mux_channel & 4);
+
+  if (settle_delay > 0)
+  {
+    delay(settle_delay);
+  }
+
+  // Data rate also affects full-scale value.
+  // uint8_t dr = 0b00; // 128 SPS, +/- 2048
+  // uint8_t dr = 0b01; // 32 SPS,  +/- 8192
+  // uint8_t dr = 0b10; // 16 SPS,  +/- 16384
+  uint8_t dr = 0b11; // 8 SPS,    +/- 32768
+
+  // uint8_t pga = 0b00; // Gain = 1
+  // uint8_t pga = 0b01; // Gain = 2
+  uint8_t pga = 0b10; // Gain = 4
+  // uint8_t pga = 0b11; // Gain = 8
+  
+  uint8_t config =
+      (1 << 7) // Start conversion
+    | (1 << 4) // Single conversion
+    | (dr << 2)
+    | (pga);
+
+  adc_i2c.write(&config, 1);
+
+  if (dr == 0b00) delayMicroseconds(8);
+  if (dr == 0b01) delayMicroseconds(32);
+  if (dr == 0b10) delayMicroseconds(63);
+  if (dr == 0b11) delayMicroseconds(125);
+
+  uint8_t output[3];
+  bool done = false;
+  for (int i = 0; i < 1000; ++i)
+  {
+    adc_i2c.read(output, 3);
+
+    done = !(output[2] & (1 << 7));
+
+    if (done)
+    {
+      break;
+    }
+    delay(1);
+  };
+  if (!done)
+  {
+    Serial.println("ADC never finished");
+    return 0;
+  }
+
+  int16_t result = (output[0] << 8) | (output[1]);
+
+  // The ADS1100 gives a proportionally smaller value at higher data rates.
+  // Scale it back up to full scale.
+  if (dr == 0b00) result *= 16;
+  if (dr == 0b01) result *= 4;
+  if (dr == 0b10) result *= 2;
+
+  return result;
+}
+
+
+
+float analogReadAverage(int pin, int n)
+{
+  uint32_t result = 0;
+  for (int i = 0; i < n; ++i)
+  {
+    result += analogRead(pin);
+  }
+  return result / (float)n;
+}
+
+
+int analogRange = 1 << 10;
+
+float readBatteryVoltage()
+{
+  float vbat = analogReadAverage(V_BAT_SENSE, 16);
+  vbat /= analogRange;
+  vbat *= 3.3;
+  vbat /= (10.0 / 110.0);
+  return vbat;
+}
+
+float readHeaterCurrent()
+{
+  float sense_resistor = 1;
+  float heater_sense = analogReadAverage(HEATER_SENSE, 16);
+  float current_mA = heater_sense / 4095.0 * 3.3 / sense_resistor * 1000.0;
+  return current_mA;
+}
+
+
+
+RTC_DS3231 rtc;
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL);
+#endif
+
+
+
+int id = 0;
+
+int analogToDigit(int pin)
+{
+  pinMode(pin, INPUT);
+  auto val = analogRead(pin);
+  float increment = analogRange / 10.0;
+  float grayArea = analogRange / 80.0;
+  for (int i = 0; i <= 9; ++i)
+  {
+    float threshold = (i + 1) * increment;
+    float lowerGrayArea = i * increment + grayArea;
+    float upperGrayArea = threshold - grayArea;
+    if (val < threshold)
+    {
+      if (i > 0 && val < lowerGrayArea)
+      {
+        Serial.printf("Warning: ID pin %i is in the lower gray area. ", pin);
+        Serial.printf("val = %i, threshold = %i, lowerGrayArea = %i\n", val, (int)threshold, (int)lowerGrayArea);
+      }
+      if (i < 9 && val > upperGrayArea)
+      {
+        Serial.printf("Warning: ID pin %i is in the upper gray area. ", pin);
+        Serial.printf("val = %i, threshold = %i, upperGrayArea = %i\n", val, (int)threshold, (int)upperGrayArea);
+      }
+      return i;
+    }
+  }
+  return 9;
+}
+
+void readId()
+{
+  id = 0;
+  id += analogToDigit(ID_0);
+  id += analogToDigit(ID_1) * 10;
+  id += analogToDigit(ID_2) * 100;
+  Serial.print("Read ID as ");
+  Serial.println(id);
+}
+
+
+void setup() {
+
+  pinMode(KEEP_ON, OUTPUT);
+  digitalWrite(KEEP_ON, 1);
+
+  pinMode(HEAT, OUTPUT);
+  digitalWrite(HEAT, 0);
+
+
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_Y, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_FEATHER, OUTPUT);
+  pinMode(LED_POWER, OUTPUT);
+  pinMode(LED_TIMER, OUTPUT);
+  pinMode(LED_ERROR, OUTPUT);
+
+  
+  pinMode(MUX_S0, OUTPUT);
+  digitalWrite(MUX_S0, 0);
+  pinMode(MUX_S1, OUTPUT);
+  digitalWrite(MUX_S1, 0);
+  pinMode(MUX_S2, OUTPUT);
+  digitalWrite(MUX_S2, 0);
+
+
+  Serial.begin(9600); // Value ignored
+  
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+  pixel.begin();
+  pixel.clear();
+  pixel.show();
+#endif
+
+  setup_adc();
+
+
+  analogReadResolution(12);
+  analogRange = 1 << 12;
+
+  pinMode(SW_AUX, INPUT_PULLUP);
+
+  pinMode(SD_CARD_DETECT, INPUT_PULLUP);
+
+  readId();
+}
+
+void loop() {
+  // Keep itself on once it starts the loop
+  digitalWrite(KEEP_ON, 1);
+  // Delay after enabling KEEP_ON is necessary, or our battery voltage reading is low.
+  // Don't know why yet.
+  delay(10);
+
+  Serial.print("Battery voltage: ");
+  Serial.println(readBatteryVoltage());
+
+  digitalWrite(LED_R, 1);
+  delay(500);
+  digitalWrite(LED_R, 0);
+  delay(500);
+  digitalWrite(LED_Y, 1);
+  delay(500);
+  digitalWrite(LED_Y, 0);
+  delay(500);
+  digitalWrite(LED_G, 1);
+  delay(500);
+  digitalWrite(LED_G, 0);
+  delay(500);
+  digitalWrite(LED_FEATHER, 1);
+  delay(500);
+  digitalWrite(LED_FEATHER, 0);
+  delay(500);
+  digitalWrite(LED_POWER, 1);
+  delay(500);
+  digitalWrite(LED_POWER, 0);
+  delay(500);
+  digitalWrite(LED_TIMER, 1);
+  delay(500);
+  digitalWrite(LED_TIMER, 0);
+  delay(500);
+  digitalWrite(LED_ERROR, 1);
+  delay(500);
+  digitalWrite(LED_ERROR, 0);
+  delay(500);
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+  pixel.setPixelColor(0, pixel.Color(16, 0, 0));
+  pixel.show();
+  delay(1000);
+  pixel.setPixelColor(0, pixel.Color(0, 16, 0));
+  pixel.show();
+  delay(1000);
+  pixel.setPixelColor(0, pixel.Color(0, 0, 16));
+  pixel.show();
+  delay(1000);
+  pixel.clear();
+  pixel.show();
+#endif
+
+
+
+  Serial.println("Thermistors: ");
+  for (int channel = 0; channel < 6; ++channel)
+  {
+    Serial.print(read_adc(channel, 1));
+    Serial.print("\t");
+  }
+  Serial.println();
+
+  if (rtc.begin())
+  {
+    // Serial.println("Found RTC");
+
+    if (rtc.lostPower())
+    {
+      Serial.println("RTC lost power");
+      rtc.adjust(DateTime(2025, 5, 28, 14, 15, 0));
+    }
+
+    char datetime[32] = "YYYY-MM-DD hh:mm:ss";
+    rtc.now().toString(datetime);
+
+    Serial.print("Current time: ");
+    Serial.println(datetime);
+
+    rtc.disable32K();
+    rtc.writeSqwPinMode(DS3231_OFF);
+
+    rtc.clearAlarm(1);
+    rtc.clearAlarm(2);
+
+    rtc.setAlarm1(DateTime(0, 0, 0, 0, 0, 0), DS3231_A1_Second);
+  }
+  else
+  {
+    Serial.println("Couldn't find RTC");
+  }
+
+
+
+  Serial.print("Heater current while off: ");
+  Serial.print(readHeaterCurrent());
+  Serial.println(" mA");
+
+  int heatSeconds = 5;
+  Serial.print("Turning heater on for ");
+  Serial.print(heatSeconds);
+  Serial.println(" seconds");
+  digitalWrite(HEAT, 1);
+  delay(heatSeconds * 1000);
+  float heaterCurrent = readHeaterCurrent();
+  float batteryVoltage = readBatteryVoltage();
+  digitalWrite(HEAT, 0);
+  Serial.println("Heater off");
+  Serial.print("Heater current: ");
+  Serial.print(heaterCurrent);
+  Serial.println(" mA");
+  Serial.print("Battery voltage while heater is on: ");
+  Serial.println(batteryVoltage);
+
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+  delay(1);
+  Serial.print("Internal temp: ");
+  Serial.print(analogReadTemp());
+  Serial.println(" C");
+#endif
+
+  readId();
+
+  if (setupSD())
+  {
+    Serial.println("Writing to SD");
+    appendToSD("Hello, world!", id);
+#ifdef ARDUINO_ADAFRUIT_FEATHER_RP2040_ADALOGGER
+    appendToSD("From RP2040", id);
+#endif
+#ifdef ADAFRUIT_FEATHER_M0
+    appendToSD("From M0", id);
+#endif
+    
+    char datetime[32] = "YYYY-MM-DD hh:mm:ss";
+    rtc.now().toString(datetime);
+    appendToSD(datetime, id);
+  }
+
+
+  if (!digitalRead(SW_AUX))
+  {
+    Serial.println("Aux switch pressed, running another cycle immediately");
+  }
+  else
+  {
+    Serial.println("Aux switch not pressed, going to sleep");
+    
+    // Turn itself off
+    digitalWrite(KEEP_ON, 0);
+    delay(1000);
+
+    // Wait for alarm
+    Serial.print("Waiting for alarm");
+    while (!rtc.alarmFired(1))
+    {
+      if (!digitalRead(SW_AUX))
+      {
+        Serial.println("Aux switch pressed, running another cycle immediately");
+        break;
+      }
+
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
